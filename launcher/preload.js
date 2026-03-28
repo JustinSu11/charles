1199f551ff82066d26eb343ab67cf49a221588a1
Justin <justin.nguyen77@yahoo.com>
2026-03-28 18:59:11 -0500

    fix: return cleanup function from preload IPC listeners to prevent leaks

--- launcher/preload.js
+++ launcher/preload.js
@@ -1,10 +1,18 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  start:          () => ipcRenderer.invoke('start-services'),
-  stop:           () => ipcRenderer.invoke('stop-services'),
-  minimize:       () => ipcRenderer.invoke('minimize-window'),
-  closeWindow:    () => ipcRenderer.invoke('close-window'),
-  onStatusUpdate: (cb) => ipcRenderer.on('status-update',      (_e, d) => cb(d)),
-  onVoiceState:   (cb) => ipcRenderer.on('voice-state-update', (_e, d) => cb(d)),
+  start:       () => ipcRenderer.invoke('start-services'),
+  stop:        () => ipcRenderer.invoke('stop-services'),
+  minimize:    () => ipcRenderer.invoke('minimize-window'),
+  closeWindow: () => ipcRenderer.invoke('close-window'),
+  onStatusUpdate: (cb) => {
+    const listener = (_e, d) => cb(d)
+    ipcRenderer.on('status-update', listener)
+    return () => ipcRenderer.removeListener('status-update', listener)
+  },
+  onVoiceState: (cb) => {
+    const listener = (_e, d) => cb(d)
+    ipcRenderer.on('voice-state-update', listener)
+    return () => ipcRenderer.removeListener('voice-state-update', listener)
+  },
 })
